bc4d4854ce4d90e59a920f84c49b930d7388b8e2
Gui Araújo <guiaraujonasc@gmail.com>
2026-02-19 23:15:50 -0300

    feat(api): add random photo loading on button click

--- src/js/script.js
+++ src/js/script.js
@@ -40,3 +40,33 @@ async function loadToday() {
     error_message.style.display = "block";
   }
 }
+
+async function loadRandom() {
+  try {
+    informations_box.style.display = "none";
+    error_message.style.display = "none"
+    loading.style.display = "block"
+    const start = new Date("1995-06-16");
+    const today = new Date();
+    
+    const randomTime = start.getTime() + Math.random() * (today.getTime() - start.getTime());
+    const randomDate = new Date(randomTime).toISOString().split("T")[0];
+    const URL2 = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${randomDate}`
+    const response = await fetch(URL2);
+    if (!response.ok) throw new Error("Houve um erro: " + response.status);
+
+    const data = await response.json();
+    image.src = data.hdurl || data.url;
+    image.alt = data.copyright || "NASA"
+    const photo_date_formatted = new Date(data.date + "T00:00:00").toLocaleDateString("pt-BR");
+    photo_date.textContent = photo_date_formatted;
+    image.onload = () => {
+      informations_box.style.display = "block";
+      loading.style.display = "none";
+    };
+  } catch(error) {
+    error_message.textContent = error.message
+    loading.style.display = "none"
+    error_message.style.display = "block";
+  }
+}
