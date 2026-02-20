86b1dcf0f97cb8178e6df1517dbbdada57c79d12
Gui Araújo <guiaraujonasc@gmail.com>
2026-02-20 00:57:51 -0300

    feat(api): add photo loading by input date

--- src/js/script.js
+++ src/js/script.js
@@ -70,3 +70,36 @@ async function loadRandom() {
     error_message.style.display = "block";
   }
 }
+
+const user_date = document.getElementById("date");
+
+user_date.addEventListener("change", async () => {
+    try {
+      informations_box.style.display = "none";
+      error_message.style.display = "none"
+      loading.style.display = "block"
+      
+      
+      
+        const input = user_date.value;
+        const URL2 = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${input}`;
+        const response = await fetch(URL2);
+        if (!response.ok) throw new Error("Houve um erro: " + response.status);
+    
+        const data = await response.json();
+        image.src = data.url;
+        image.alt = data.copyright || "NASA"
+        const photo_date_formatted = new Date(data.date + "T00:00:00").toLocaleDateString("pt-BR");
+        photo_date.textContent = photo_date_formatted;
+        image.onload = () => {
+          informations_box.style.display = "block";
+          loading.style.display = "none";
+        };      
+      
+      
+    } catch(error) {
+      error_message.textContent = error.message
+      loading.style.display = "none"
+      error_message.style.display = "block";
+    }
+})
